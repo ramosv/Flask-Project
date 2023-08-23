bd2d5233e7fbd67aad70be0ec766b8d70c4170d0
ramosv <56368263+ramosv@users.noreply.github.com>
2023-08-23 09:50:04 -0600

    Bugs

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 ## Import site package -> why __init__.py was needed in the folder structure
 
 import os, sys
-#sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-sys.path.append("c:/Users/ramosv/Desktop/Python_Flask_2.0/Flask-Project")
-
-from .site import build
+sys.path.append(os.path.join(os.path.dirname(__file__),"site"))
 
 
-app = build()
 
-if __name__ == "main":
-    app.run(debug=True)
+from .site.app import app
+
+
+App = app.build()
+
+if __name__ == "__main__":
+    App.run(debug=True)
